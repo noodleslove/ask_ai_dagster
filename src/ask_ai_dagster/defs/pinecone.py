6dd6f7e4086ea1e0195dabcbd74e4ef30740d99f
Eddie Ho <noodleslove@icloud.com>
2025-12-18 20:05:33 -0800

    feat: add pinecone

---
+++ src/ask_ai_dagster/defs/pinecone.py
@@ -0,0 +1,26 @@
+from typing import Optional
+import dagster as dg
+
+from pinecone import Pinecone
+
+
+class PineconeResource(dg.ConfigurableResource):
+    """Resource for storing and querying vectors with Pinecone."""
+
+    pinecone_api_key: str
+    openai_api_key: str
+
+    def setup_for_execution(self, context: dg.InitResourceContext) -> None:
+        self._pinecone = Pinecone(api_key=self.pinecone_api_key)
+
+    def create_index(self, index_name: str, dimension: int = 1536) -> None:
+        if index_name not in self._pinecone.list_indexes().names():
+            self._pinecone.create_index(
+                name=index_name,
+                dimension=dimension,
+                metric="cosine",
+                spec={"serverless": {"cloud": "aws", "region": "us-east-1"}},
+            )
+
+    def get_index(self, index_name: str):
+        return self._pinecone.Index(index_name)
